687e51ad136587b341699e189cffc9ebb14b69c1
ortizdavid <ortizaad1994@gmail.com>
2025-12-30 10:41:33 +0100

    added EnvConfig to rabbitmq settings

--- src/common/messaging/rabbitmq/config.py
+++ src/common/messaging/rabbitmq/config.py
@@ -1,5 +1,6 @@
 from enum import StrEnum
 from pydantic import BaseModel
+from src.common.config.env_config import EnvConfig
 
 
 class ExchangeType(StrEnum):
@@ -9,11 +10,11 @@ class ExchangeType(StrEnum):
     HEADERS = "headers"
 
 class RabbitMQConfig(BaseModel):
-    host: str = "localhost"
-    port: int = 5672
-    user: str = "guest"
-    password: str = "guest"
-    virtual_host: str = "/"
+    host: str = EnvConfig.RABBITMQ_HOST
+    port: int = EnvConfig.RABBITMQ_PORT
+    user: str = EnvConfig.RABBITMQ_USER
+    password: str = EnvConfig.RABBITMQ_PASSWORD
+    virtual_host: str = EnvConfig.RABBITMQ_VIRTUAL_HOST
 
     def connection_string(self) -> str:
         return f"amqp://{self.user}:{self.password}@{self.host}:{self.port}/{self.virtual_host.lstrip('/')}"
